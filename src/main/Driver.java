3d73d8b6d09294ff8e026ad9ed411d0952002eb6
Jay <jaykch@outlook.com>
2019-04-02 15:30:10 +1100

    created initial structure

---
+++ src/main/Driver.java
@@ -0,0 +1,17 @@
+package main;
+import app.Car;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Code car class
+		// TODO Code booking class
+		// TODO Code user class
+		// TODO Import Utilities
+
+		Car mustang =  new Car("asdf","b","c", "true",2);
+		System.out.println(mustang);
+
+	}
+
+}
